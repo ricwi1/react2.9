2bfde3de10b703408f137a0c1bb216a9eceaa388
ricwi1 <lunichgtgmail.com@MacBook-Air-LIUGAI.local>
2025-12-10 22:58:32 +0900

    header jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,22 @@
-const Header = () => { 
+const Title = () => { 
     return <h1>todos</h1>;
 };
+
+const NewTaskForm = ({ className, placeholder}) => {
+    return <input className={className} placeholder ={placeholder} />
+};
+
+function Header() {
+    return (
+        <header class="header">
+            <Title />
+            <NewTaskForm 
+                className="new-todo" 
+                placeholder ="What needs to be done?" 
+                autoFocus
+            />
+        </header>
+    )
+}
 
 export default Header
