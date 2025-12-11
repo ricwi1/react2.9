5f2020d9e5959633be2928f435414929f8f85e9c
ricwi1 <lunichgtgmail.com@MacBook-Air-LIUGAI.local>
2025-12-11 19:08:54 +0900

    editing task jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,20 +4,26 @@ function Task({ created, status, description }) {
     
     const date = created ? new Date(created) : new Date();
     const result = isNaN(date) ? "Invalid date" : formatDistanceToNow(date, { addSuffix: true });
-        
-    return (
-        <li className={status}>
-            <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>
-                    <span className="description">{description}</span>
-                    <span className="created">{result}</span>
-                </label>
-                <button className="icon icon-edit"></button>
-                <button className="icon icon-destroy"></button>
-            </div>
-        </li>
-    );
+    
+     const isEditing = status === "editing";
+
+  return (
+    <li className={status}>
+      <div className="view">
+        <input className="toggle" type="checkbox" />
+        <label>
+          <span className="description">{description}</span>
+          <span className="created">{result}</span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+
+      {isEditing && (
+        <input type="text" className="edit" defaultValue="Editing task" />
+      )}
+    </li>
+  );
 }
 
 export default Task
